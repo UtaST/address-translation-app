a3c2f9e867a4c1a1e3668f403316d795b5001884
UtaST <yutaxperia16@gmail.com>
2020-09-21 15:43:12 +0900

    addressesコントローラー作成、トップページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "addresses#index"
+  resources :addresses, only: :index
 end
