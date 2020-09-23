a0e96862416ca661acbc5b434d03cb082b26c51e
UtaST <yutaxperia16@gmail.com>
2020-09-23 16:36:36 +0900

    バリデーション追記、エラーメッセージを日本語に変換

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,8 @@
 class AddressesController < ApplicationController
   
   def index
-    @address = User.find(current_user.id).address
+    if user_signed_in?
+      @address = User.find(current_user.id).address
+    end
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,7 @@ class Address < ApplicationRecord
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "を正しく入力してください"}
+    validates :prefecture
     validates :city
     validates :house_number
   end
