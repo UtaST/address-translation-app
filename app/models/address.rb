48e0b0ee31b7d57b13c69231a4e7609fdc298216
UtaST <yutaxperia16@gmail.com>
2020-09-23 12:25:43 +0900

    addressモデルのバリデーション 設定、optional: trueを設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  with_options presences: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "を正しく入力してください"}
+    validates :city
+    validates :house_number
+  end
 end
