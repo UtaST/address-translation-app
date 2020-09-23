525ae7a8ab2d22797e1acaf0e369b9d2ea4fb4ba
UtaST <yutaxperia16@gmail.com>
2020-09-23 12:26:27 +0900

    スペルミスを修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
 
-  with_options presences: true do
+  with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "を正しく入力してください"}
     validates :city
     validates :house_number
