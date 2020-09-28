9aeaff58c78ad185d760f1c05823e1e6251dac95
UtaST <yutaxperia16@gmail.com>
2020-09-28 19:13:19 +0900

    部分テンプレートを適用、バリデーション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,8 +3,9 @@ class Address < ApplicationRecord
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{7}\z/, message: "を正しく入力してください"}
-    validates :prefecture_code
     validates :city
     validates :house_number
   end
+
+  validates :prefecture_code, numericality: { other_than: 0, message: "を選択してください" }
 end
