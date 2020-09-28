8285d6b4138942f61956df939a6d9f4851666d9a
UtaST <yutaxperia16@gmail.com>
2020-09-28 12:13:28 +0900

    addressesテーブルのカラム型、カラム名を修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,8 +2,8 @@ class Address < ApplicationRecord
   belongs_to :user, optional: true
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "を正しく入力してください"}
-    validates :prefecture
+    validates :postal_code, format: { with: /\A\d{7}\z/, message: "を正しく入力してください"}
+    validates :prefecture_code
     validates :city
     validates :house_number
   end
