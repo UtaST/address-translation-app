4e8e9f60862ed9fad5f510c10e235cf7486a8317
UtaST <yutaxperia16@gmail.com>
2020-10-02 20:02:59 +0900

    コピーメッセージフェードイン、フェードアウト機能実装

--- app/javascript/clipboard.js
+++ app/javascript/clipboard.js
@@ -3,7 +3,17 @@ window.addEventListener("load", (e) => {
   for (let i = 0; i < copyBtn.length; i++ ) {
     copyBtn[i].addEventListener("click", (e) => {
       const copyText = document.getElementsByClassName("en-address")[i].innerText;
+      // コピーメッセージをフェードイン・フェードアウトするための準備
+      const copyMessage = document.getElementById("clip-message");
       navigator.clipboard.writeText(copyText);
+      copyMessage.style.display = "flex";
+      copyMessage.animate([{opacity: '0'}, {opacity: '1'}], 500);
+      setTimeout(function() {
+        copyMessage.animate([{opacity: "1"}, {opacity: "0"}], 500);
+        setTimeout(function() {
+          copyMessage.style.display = "none";
+        }, 500);
+      }, 3000);
     });
   };
 });
