88fce5c1f673d09459b2bcf0663e5f670bba68b3
UtaST <yutaxperia16@gmail.com>
2020-10-06 12:27:38 +0900

    `miyabi`、`tomaji`を用いた処理に変更

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -13,35 +13,30 @@ class AddressesController < ApplicationController
   private
 
   def address_params
-    params.require(:address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :room_number)
+    params.require(:address).permit(:postal_code, :prefecture, :city, :town_name, :house_number, :building_name, :room_number)
   end
 
   def address_translation(address)
-    require 'kakasi'
+    require 'miyabi'
+    require 'romaji/core_ext/string'
     # 「部屋番号」変換
     address.room_number.to_s.insert(0, '#')
 
     # 「建物名」変換
-    en_building_name = Kakasi.kakasi('-Ja -Ha -Ka', address.building_name)
+    en_building_name = address.building_name.to_kanhira.romaji.capitalize
     if en_building_name.match(/biru\z/) # 「〜ビル」の場合
       en_building_name[-4, 4] = '-building'
     elsif en_building_name.match(/manshon\z/) # 「〜マンション」の場合
       en_building_name.insert(-8, '-')
-    elsif en_building_name.include?("apa^to") # 「〜アパート」の場合
+    elsif en_building_name.include?("apa-to") # 「〜アパート」の場合
       en_building_name[-6, 6] = '-apartment'
     end
-    address.building_name = en_building_name.capitalize! # 頭文字を大文字に変換
+    address.building_name = en_building_name # 頭文字を大文字に変換
 
-    # 「町名・番地・号」変換
-    en_house_number = Kakasi.kakasi('-Ja -Ha -Ka', address.house_number).capitalize!
-    en_house_number_len = en_house_number.length # 翻訳した町名・番地・号の文字数
-    en_house_number_len.times do |i|
-      if en_house_number[i].match(/[a-z]/i) && en_house_number[i + 1].match(/\d/) # 「丁目・番地・号」部分と「町名」部分を分離、順番入れ替え
-        house_number_num = en_house_number.slice!((i + 1)..-1) # 分離した「丁目・番地・号」
-        en_house_number.insert(0, "#{house_number_num} ") # 「丁目・番地・号」を「町名」の前に挿入
-      end
-    end
-    address.house_number = en_house_number
+    # 「町名」変換
+    en_town_name = address.town_name.to_kanhira.romaji.capitalize
+    address.town_name = en_town_name
+    binding.pry
 
     # 「市区町村」変換
     city_len = address.city.length
@@ -49,15 +44,15 @@ class AddressesController < ApplicationController
     if address.city.include?("市") && address.city[-1] == "区" # 〇〇市〇〇区の場合true
       slice_point = address.city.index("市") 
       shi = address.city.slice!(0..slice_point) # 「〜市」と「〜区」を分離
-      en_shi = Kakasi.kakasi('-Ja -Ha -Ka', shi.insert(-2, "-")).capitalize! # 〜市を英訳
-      en_city = "#{en_shi} " + Kakasi.kakasi('-Ja -Ha -Ka', address.city.insert(-2, "-")).capitalize! # 〜区を英訳、統合
+      en_shi = shi.insert(-2, "-").to_kanhira.romaji.capitalize! # 〜市を英訳
+      en_city = "#{en_shi} " + address.city.insert(-2, "-").to_kanhira.romaji.capitalize! # 〜区を英訳、統合
     elsif address.city[-1] == "町" || address.city[-1] == "村" # 〇〇郡〇〇町(村)の場合true
       slice_point = address.city.index("郡")
       gun = address.city.slice!(0..slice_point) # 「〜郡」と「〜町(村)」を分離
-      en_gun = Kakasi.kakasi('-Ja -Ha -Ka', gun.insert(-2, "-")).capitalize! # 〜郡を英訳
-      en_city = "#{en_gun} " + Kakasi.kakasi('-Ja -Ha -Ka', address.city.insert(-2, "-")).capitalize! # 〜町(村)を英訳、統合
+      en_gun = gun.insert(-2, "-").to_kanhira.romaji.capitalize! # 〜郡を英訳
+      en_city = "#{en_gun} " + address.city.insert(-2, "-").to_kanhira.romaji.capitalize! # 〜町(村)を英訳、統合
     else # それ以外の翻訳
-      en_city = Kakasi.kakasi('-Ja -Ha -Ka', address.city.insert(-2, "-")).capitalize!
+      en_city = address.city.insert(-2, "-").to_kanhira.romaji.capitalize!
     end
     address.city = en_city
 
@@ -65,7 +60,7 @@ class AddressesController < ApplicationController
     unless address.prefecture == "北海道" # 北海道以外の場合true
       address.prefecture.insert(-2, "-") # 「県・府・都」の前に"-(ハイフン)"を挿入
     end
-    en_prefecture = Kakasi.kakasi('-Ja -Ha -Ka', address.prefecture).capitalize! # 翻訳
+    en_prefecture = address.prefecture.to_kanhira.romaji.capitalize! # 翻訳
     address.prefecture = en_prefecture
   end
 end
