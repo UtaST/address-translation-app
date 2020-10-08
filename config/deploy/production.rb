14375065e9e28cbde9d98f9d9de6a593982f5b12
UtaST <yutaxperia16@gmail.com>
2020-10-09 00:49:28 +0900

    Elastic IP再設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.193.80.157', user: 'ec2-user', roles: %w{app db web}
+server '54.95.243.100', user: 'ec2-user', roles: %w{app db web}
