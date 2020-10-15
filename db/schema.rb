bbd4455e40f35e40ff45dfc663004a744a6ed1bd
UtaST <yutaxperia16@gmail.com>
2020-10-16 00:18:03 +0900

    postal_codeカラムのデータ型をstringに変更

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_09_22_091202) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "postal_code", null: false
+    t.string "postal_code", null: false
     t.string "prefecture", null: false
     t.string "city", null: false
     t.string "town_name", null: false
